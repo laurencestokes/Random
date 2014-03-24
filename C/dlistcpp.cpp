c285321aaf26a4c75b6f176fb06613d25c880aa9
LaurenceStokes <laurence_stokes@hotmail.com>
2014-03-24 23:50:09 +0000

    Create dlistcpp.cpp

---
+++ C/dlistcpp.cpp
@@ -0,0 +1,36 @@
+//Converting a doubly linked list C implementation to C++, replacing void pointer polymorphism with 
+//Object-Orientated polymorphism paradigms in C++, namely templates.
+
+#include <iostream>
+
+using namepsace std;
+
+template<class T>
+class doublylinkedlist
+{
+private:
+	struct node
+	{
+		T data;
+		node *prev;
+		node *next;
+	}
+public:
+	doublylinkedlist(){}
+	
+	//class methods to be implemented outside of class definition
+	void append(T);
+	void push(T);
+	void pop();
+};
+
+template<class T> 
+void doublylinkedlist<T>::append(T item)
+{ 
+	node *p=new node; 
+	p->data=item; 
+	p->prev=tail; 
+	p->next=NULL; 
+	tail=p; 
+}
+	
