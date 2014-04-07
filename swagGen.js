eda6b10f6b410ebd394a11b427c243380512c89e
LaurenceStokes <laurence_stokes@hotmail.com>
2014-04-07 21:23:47 +0100

    Update swagGen.js
    Added some additional features to the name gen (saving previously generated names and selected user chosen favourite names)

--- swagGen.js
+++ swagGen.js
@@ -8,23 +8,42 @@
  * ======================================================================== */
 
 	//Set up Global Variables (the swag name arrays)
-	//These would normally be different. Certain strings can only be at the start (# etc)
-	var mySwagArray1 = ["swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14",  "destroyer",  "XxX", "pimping", "tupac"];
-	var mySwagArray2 = ["swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14",  "destroyer",  "XxX", "pimping", "tupac"];
-	var mySwagArray3 = ["swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14",  "destroyer",  "XxX", "pimping", "tupac"];
 
-
+	var mySwagArray1 = ["#", "swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14", "cunt", "destroyer", "arseface", "XxX", "pimping", "tupac"];
+	var mySwagArray2 = ["swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14", "cunt", "destroyer", "arseface", "pimping", "tupac"];
+	var mySwagArray3 = ["swag","yolo", "420", "blaseit", "fgt", "cool", "hipster", "overload", "2k14", "cunt", "destroyer", "arseface", "XxX", "pimping", "tupac"];
 	
+	//html element to update the result to
+	var elementToUpdate = "header";
+	
+	//html element to store all the generated names
+	var allNames = "allNames";
+	
+	//html button element
+	var button = "clickme";
+	
+	//html button element
+	var favourites = "favourites";
+	
+	//favourites button element
+	var favouriteButton = "favouriteButton"
+	
+	//array to store the previously generated names
+	var nameArray = new Array();
+
+	//array to store favourites
+	var favs = new Array();
+	
+	/**
+	function that generates the name.
+	**/
 	function genName(){
-		
-		//html element to update the result to
-		var elementToUpdate = "header";
-		
+
 		//result string
 		var result = "";
 		//curent swag word string
 		var swag = "";
-		
+
 		//iterate through the arrays 3 times to construct a random swagName.
 		for (var i = 0; i <= 2; i++)
 			{	
@@ -45,13 +64,60 @@
 			}else if(i === 2){
 				//extract a random swag name from array3 to append to result
 				swag = mySwagArray3[Math.floor(Math.random()*mySwagArray3.length)];
-				//append extracted name to result if it's not already in result
-				if (result.indexOf(swag) > -1) {
+				//append extracted name to result if it's not already in result - XxX can be first and third indice, however.
+				if (result.indexOf(swag) > -1 && swag != 'XxX') {
 					i--;
 				} else {
 					result = result + " " + swag;
 				}
 			}
 		}
+		setName(result);
+		saveName(result, 10)
+		updateButton();
+		enableButton();
+	}
+	
+	//function to set the desired html element to the generated swagName result.
+	function setName(result){
 		document.getElementById(elementToUpdate).innerHTML= result;
+	}
+	
+	/**
+	Function that saves all the generated names, up to a specified array size
+	e.g a size of 5 will allow for 5 elements to be saved at once. specifying no 
+	size allows (infinite) elements
+	**/
+	function saveName(result, size){
+		nameArray.unshift(result);
+		var output = new Array(size);
+		output = (nameArray.slice(0, size)).join(' |');
+		document.getElementById(allNames).innerHTML=output;
+	}
+
+	//updates the Button after the first click from 'Click me' to 'Another'
+	function updateButton(){
+		document.getElementById(button).innerHTML= "Another?";
+	}
+	
+	/**
+	Adds a generatedName to a list of favourites. Once added, disabled the button
+	so the same name cannot be added again.
+	Also checks that at least one name has been generated (nameArray[0] is not null).
+	**/
+	function addFavourite(){
+		if(nameArray[0] != null){
+			if(favs.indexOf(nameArray[0]) < 0){
+				favs.unshift(nameArray[0]);
+				document.getElementById(favourites).innerHTML= favs.join(' |');
+				document.getElementById(favouriteButton).disabled = true; 
+			}
+		}else{
+			window.alert("You need to create a name before you can save a favourite...");
+		}
+	}
+	
+	//enablesButton if it's otherwise disabled
+	function enableButton(){
+		document.getElementById(favouriteButton).disabled = false;
 	}
